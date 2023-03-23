65b469fec285534597ba5862ac8cba3805c51526
gabumwaingo <gabumwaingo@gmail.com>
2023-03-23 08:25:47 -0700

    one

--- monty.h
+++ monty.h
@@ -49,7 +49,7 @@ void parse_command(stack_t **stack, char *op, unsigned int line_num);
 
 
 void push(stack_t **stack, unsigned int number);
-void pall(stack_t **stack, unsigned int line_number);
+void pall(stack_t **stack, unsigned int number);
 
 
 #endif
