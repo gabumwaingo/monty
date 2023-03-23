92d25fd47682da61fa01154c4159d7395765db44
gabumwaingo <gabumwaingo@gmail.com>
2023-03-23 07:52:27 -0700

    Task 1

---
+++ monty.h
@@ -0,0 +1,66 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <stdlib.h>
+#include <stdio.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <ctype.h>
+
+#include <stdbool.h>
+#include <string.h>
+
+#define DELIMS "\n \r\t"
+
+/**
+ * struct stack_s - doubly linked list representation of a stack (or queue)
+ * @n: integer
+ * @prev: points to the previous element of the stack (or queue)
+ * @next: points to the next element of the stack (or queue)
+ *
+ * Description: doubly linked list node structure
+ * for stack, queues, LIFO, FIFO
+ */
+typedef struct stack_s
+{
+        int n;
+        struct stack_s *prev;
+        struct stack_s *next;
+} stack_t;
+/**
+ * struct instruction_s - opcode and its function
+ * @opcode: the opcode
+ * @f: function to handle the opcode
+ *
+ * Description: opcode and its function
+ * for stack, queues, LIFO, FIFO
+ */
+typedef struct instruction_s
+{
+        char *opcode;
+        void (*f)(stack_t **stack, unsigned int line_number);
+} instruction_t;
+
+extern stack_t **global_head;
+void global_free(void);
+void read_file(char *file, stack_t **stack);
+void parse_command(stack_t **stack, char *op, unsigned int line_num);
+
+
+void push(stack_t **stack, unsigned int number);
+void pall(stack_t **stack, unsigned int line_number);
+
+
+#endif
+
+
+
+
+
+
+
+
+
+
+
---
+++ push.c
@@ -0,0 +1,36 @@
+#include "monty.h"
+
+/**
+ * push - adds elements to the stack
+ * @stack: pointer to the head of the head of the stack
+ * @number: the elementto be added
+ * Return: nothing
+ */
+void push(stack_t **stack, unsigned int number)
+{
+	stack_t *node;
+	char *num;
+
+	num = strtok(NULL, DELIMS);
+	if (num == NULL)
+	{
+		printf("L%u: usage: push integer\n",_number);
+		exit(EXIT_FAILURE);
+	}
+
+	node = malloc(sizeof(stack_t));
+	if (node == NULL)
+	{
+		printf("Error: malloc failed\n");
+		exit(EXIT_FAILURE);
+	}
+
+	node->n = atoi(num);
+	node->prev = NULL;
+	node->next = *stack;
+
+	if (*stack != NULL)
+		(*stack)->prev = node;
+
+	*stack = node;
+}
